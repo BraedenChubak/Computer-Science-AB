49f4fb52e69c1208b8d9e7955a9b4a9daaade193
BraedenChubak <chubak.b@stu.janesville.k12.wi.us>
2024-09-12 11:18:56 -0500

    9/12

--- src/Q1/Prog505b.java
+++ src/Q1/Prog505b.java
@@ -2,18 +2,37 @@ package Q1;
 
 import Utils.Console;
 
+import javax.swing.plaf.basic.BasicBorders;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Prog505b {
     static class Baseball {
-        private int[][] HandB;
-
-        public Baseball() {
-
+        private int[][] hitsAndBats;
+        private double[] hitRate;
+        private int[] hitDays;
+        private int sumHits;
+        public Baseball(int[][]hab) {
+            hitsAndBats = hab;
+            hitRate = new double[7];
+            sumHits = 0;
         }
-    }
+
+        public void calc() {
+            for (int i = 0; i < hitRate.length; i++) {
+                hitRate[i] = hitsAndBats[i][0] / (double)hitsAndBats[i][1];
+                sumHits += hitsAndBats[i][0];
+            }
+        }
+
+        public double[] getHitRate() {
+            return hitRate;
+        }
+        public int getSumHits() {
+            return sumHits;
+        }
+     }
     public static void main(String[] args) {
         int[][] hitsAndBats = new int[7][2];
         String[] days = new String[7];
@@ -39,13 +58,14 @@ public class Prog505b {
                 }
                 System.out.println();
             }
+            Baseball ball = new Baseball(hitsAndBats);
+            ball.calc();
 
             for (int i = 0; i < 7; i++) {
-                System.out.println(days[i] + ": " + (double)Math.round(hitsAndBats[i][0]/(double)hitsAndBats[i][1] * 1000) / 1000);
+                System.out.printf("%s\t%d\t%.3f\n", days[i], hitsAndBats[i][0], ball.getHitRate()[i]);
             }
-            int sum = hitsAndBats[0][0] + hitsAndBats[1][0] + hitsAndBats[2][0] + hitsAndBats[3][0] + hitsAndBats[4][0] + hitsAndBats[5][0] + hitsAndBats[6][0];
 
-            System.out.println("Total Hits: " + sum);
+            System.out.println("Total Hits: " + ball.getSumHits());
 
             file.close();
 
@@ -63,12 +83,12 @@ Ruben Romper	   0.0	0.333	0.5	0.4	0.286	0.375	0.0
 Lisa Longball	   1.0	0.5	0.714	0.25	0.75	0.5	0.2
 Claire Corner	   0.25	0.667	0.375	0.444	0.8	0.333	0.2
 Jared Just	   1.0	0.5	0.4	0.286	0.125	1.0	0.333
-Monday: 0.471
-Tuesday: 0.514
-Wednesday: 0.522
-Thursday: 0.396
-Friday: 0.356
-Saturday: 0.395
-Sunday: 0.324
+Monday	8	0.471
+Tuesday	19	0.514
+Wednesday	24	0.522
+Thursday	19	0.396
+Friday	16	0.356
+Saturday	15	0.395
+Sunday	12	0.324
 Total Hits: 113
  */
