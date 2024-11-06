c3bf601694b60e7fe83e28a3636ae62fc88b0433
BraedenChubak <chubak.b@stu.janesville.k12.wi.us>
2024-11-06 11:27:52 -0600

    11/6

---
+++ src/Q2/DequeLab.java
@@ -0,0 +1,43 @@
+package Q2;
+
+import DataStructures.Deque;
+
+public class DequeLab {
+    public static void main(String[] args) {
+        var deque = new Deque<Integer>();
+        for (int i = 0; i < 10; i++) {
+            deque.enqueueFront(i);
+        }
+        for (int i = 10; i < 20; i++) {
+            deque.enqueueRear(i);
+        }
+        System.out.println(deque.size());
+        System.out.println(deque.peekFront());
+        System.out.println(deque.peekRear());
+        System.out.println(deque.dequeueFront());
+        System.out.println(deque.dequeueRear());
+        System.out.println(deque.size());
+
+        var rand = new java.util.Random(42);
+        for (int i = 0; i < 50; i++) {
+            deque.enqueueFront(rand.nextInt(50));
+        }
+        for (int i = 0; i < 48; i++) {
+            deque.dequeueFront();
+        }
+        System.out.println("New Size: " + deque.size());
+        System.out.println(deque.peekFront());
+        System.out.println(deque.peekRear());
+    }
+}
+/*
+20
+9
+19
+9
+19
+18
+New Size: 20
+13
+18
+ */
