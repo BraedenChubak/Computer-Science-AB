ad10c53e6baf5bf849d9db68fb0351707211e817
BraedenChubak <chubak.b@stu.janesville.k12.wi.us>
2025-04-04 11:21:57 -0500

    4/4

--- src/Q3/Prog850b.java
+++ src/Q3/Prog850b.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Prog850a {
+public class Prog850b {
     public static void main(String[] args) {
         try {
             var file = new Scanner(new File("Langdat/prog850b.txt"));
